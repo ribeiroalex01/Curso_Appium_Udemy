ec3f6554185575dccfc7d40d0c26eeb07b70c6e2
Alex Bruno <alex.bruno0102@hotmail.com>
2022-04-20 11:54:36 -0300

    Initial commit

---
+++ app/src/main/java/com/br/ciandt/appium/CalculatorTest.kt
@@ -0,0 +1,38 @@
+package com.br.ciandt.appium
+
+/*import io.appium.java_client.MobileElement
+import io.appium.java_client.android.AndroidDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
+
+
+
+class CalculatorTest {
+
+    fun main() {
+        val desiredCapabilities = DesiredCapabilities ()
+        desiredCapabilities.setCapability("platformName", "Android")
+        desiredCapabilities.setCapability("appium:deviceName", "Nexus 5X API 30")
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2")
+        desiredCapabilities.setCapability("appium: appPackage", "com.android.calculator2")
+        desiredCapabilities.setCapability("appium: appActivity", "com.android.calculator2.Calculator")
+
+        val driver = AndroidDriver <MobileElement>(URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities)
+
+
+        val el1: MobileElement  = driver.findElementById("com.google.android.calculator:id/digit_2")
+        el1.click()
+        val el2: MobileElement = driver.findElementByAccessibilityId("plus")
+        el2.click()
+        val el3: MobileElement = driver.findElementById("com.google.android.calculator:id/digit_2")
+        el3.click()
+        val el4: MobileElement = driver.findElementById("com.google.android.calculator:id/result_preview")
+        el4.click()
+
+        driver.close()
+
+    }
+
+
+}*/
---
+++ app/src/main/java/com/br/ciandt/curso_appium_udemy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.br.ciandt.curso_appium_udemy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
